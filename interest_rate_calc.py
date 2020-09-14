69fb6566119cf9974062b94db4bc19682ad8e2ba
Moataz-AbdElKhalek <Moataz-AbdElKhalek@users.noreply.github.com>
2020-09-13 21:35:35 -0500

    updating script with user interactions

--- interest_rate_calc.py
+++ interest_rate_calc.py
@@ -1,16 +1,25 @@
-from matplotlib import pyplot as plt
 import pandas as pd
-initial = 1000
-interest = .15
-ir = pd.DataFrame ({'year': range(1, 21)})
+
+initial = float(input("Enter your initial saving value >> "))
+interest = float(input("Enter the current interest rate >> "))
+years = int(input("Enter the number of years >> "))
+ir = pd.DataFrame ({'year': range(1, years+1)})
 
 ir['final'] = initial * ((1+interest)**ir['year'])
 
 print (ir)
 
+print ('\nTotal Money Gained at the End of Period >> ', round(ir.at[years-1,'final']-initial , 2) )
+
 # Plot the line
-# %matplotlib inline
-plt.plot(ir.year, ir.final, color="green")
-plt.xlabel('Year')
-plt.ylabel('Final')
-plt.show()
+
+graph = input("Do you want to graph output? (Y,N) >> ")
+if graph == 'Y' or graph == 'y' or graph == 'Yes' or graph == 'yes':
+
+    from matplotlib import pyplot as plt
+    plt.plot(ir.year, ir.final, color="green")
+    plt.xlabel('Year')
+    plt.ylabel('Final')
+    plt.show()
+
+print('\nThanks for using the Interest Rate Calculator!\n')
